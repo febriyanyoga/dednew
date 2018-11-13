2bfcec2843e5807b4a1ec009145ebdc71833cbf3
febriyanyoga <febriyanyoga@gmail.com>
2018-11-14 06:29:48 +0700

    database

--- ded_new.sql
+++ ded_new.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.6.6
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 13, 2018 at 03:33 AM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.11
+-- Host: localhost
+-- Generation Time: 13 Nov 2018 pada 23.28
+-- Versi Server: 5.7.17-log
+-- PHP Version: 5.6.30
 
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -24,7 +23,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `captcha`
+-- Struktur dari tabel `captcha`
 --
 
 CREATE TABLE `captcha` (
@@ -32,78 +31,65 @@ CREATE TABLE `captcha` (
   `captcha_time` int(10) NOT NULL,
   `ip_address` varchar(16) NOT NULL,
   `word` varchar(20) NOT NULL
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `captcha`
+-- Dumping data untuk tabel `captcha`
 --
 
 INSERT INTO `captcha` (`captcha_id`, `captcha_time`, `ip_address`, `word`) VALUES
-(1, 1541390705, '::1', '05929635'),
-(2, 1541390744, '::1', '48372260'),
-(3, 1541390777, '::1', '57230772'),
-(4, 1541390941, '::1', '26142900'),
-(5, 1541390960, '::1', '54241660'),
-(6, 1541390969, '::1', '83781750'),
-(7, 1541390999, '::1', '09083178');
+(41, 1542151605, '::1', '92732964');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_ded`
---
-
-CREATE TABLE `tbl_ded` (
-  `id_ded` int(15) NOT NULL,
-  `id_tupoksi` int(15) NOT NULL,
-  `objek_data` varchar(200) NOT NULL
-) ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_dokumen`
+-- Struktur dari tabel `tbl_dokumen`
 --
 
 CREATE TABLE `tbl_dokumen` (
   `id_dokumen` int(11) NOT NULL,
-  `id_ded` int(11) NOT NULL,
+  `id_skpa` int(11) NOT NULL,
   `nama_dokumen` varchar(200) NOT NULL,
   `nama_file` varchar(100) NOT NULL
-) ;
-
---
--- Dumping data for table `tbl_dokumen`
---
-
-INSERT INTO `tbl_dokumen` (`id_dokumen`, `id_ded`, `nama_dokumen`, `nama_file`) VALUES
-(8, 1, 'tes', '618e60dacc29fa45e8d89996d8573bad.pdf');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_note`
+-- Struktur dari tabel `tbl_note`
 --
 
 CREATE TABLE `tbl_note` (
   `id_note` int(10) NOT NULL,
-  `id_ded` int(10) NOT NULL,
+  `id_skpa` int(10) NOT NULL,
   `judul` varchar(100) NOT NULL,
   `note` varchar(1000) NOT NULL,
   `tanggal` date NOT NULL
-) ;
-
---
--- Dumping data for table `tbl_note`
---
-
-INSERT INTO `tbl_note` (`id_note`, `id_ded`, `judul`, `note`, `tanggal`) VALUES
-(2, 1, 'coba', 'asdaa', '2018-10-19');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_organisasi`
+-- Struktur dari tabel `tbl_objek_data`
+--
+
+CREATE TABLE `tbl_objek_data` (
+  `id_objek_data` int(15) NOT NULL,
+  `id_suborganisasi` int(15) NOT NULL,
+  `objek_data` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `tbl_objek_data`
+--
+
+INSERT INTO `tbl_objek_data` (`id_objek_data`, `id_suborganisasi`, `objek_data`) VALUES
+(1, 1, 'ip_khskdddddd');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_organisasi`
 --
 
 CREATE TABLE `tbl_organisasi` (
@@ -112,12 +98,42 @@ CREATE TABLE `tbl_organisasi` (
   `nama_organisasi` varchar(100) NOT NULL,
   `tugas` varchar(2000) NOT NULL,
   `fungsi` varchar(2000) NOT NULL
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `tbl_organisasi`
+--
+
+INSERT INTO `tbl_organisasi` (`id_organisasi`, `id_skpa`, `nama_organisasi`, `tugas`, `fungsi`) VALUES
+(4, 1, 'karang taruna', '<ol><li>oke</li><li>iya</li><li>&nbsp;</li></ol>', '<ol><li>iyes</li><li>oke</li><li>&nbsp;</li></ol>'),
+(5, 3, 'Pokmaswas Hamengkubengawan', '<ol><li>bersih bersih sungai</li></ol>', '<ol><li>merawat sungai</li></ol>'),
+(6, 1, 'Pokmaswas Hamengkubengawan', '<p>1</p>', '<p>1</p>');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_profil`
+-- Struktur dari tabel `tbl_parameter`
+--
+
+CREATE TABLE `tbl_parameter` (
+  `id_parameter` int(12) NOT NULL,
+  `id_objek_data` int(15) NOT NULL,
+  `nama_parameter` varchar(11) NOT NULL,
+  `tipe_data` enum('string','numeric','text','date') NOT NULL,
+  `panjang_data` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `tbl_parameter`
+--
+
+INSERT INTO `tbl_parameter` (`id_parameter`, `id_objek_data`, `nama_parameter`, `tipe_data`, `panjang_data`) VALUES
+(1, 1, 'username', 'text', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_profil`
 --
 
 CREATE TABLE `tbl_profil` (
@@ -128,10 +144,10 @@ CREATE TABLE `tbl_profil` (
   `kabupaten` varchar(50) NOT NULL,
   `no_telp` varchar(50) NOT NULL,
   `logo` varchar(50) NOT NULL
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `tbl_profil`
+-- Dumping data untuk tabel `tbl_profil`
 --
 
 INSERT INTO `tbl_profil` (`id`, `nama_sistem`, `alamat`, `provinsi`, `kabupaten`, `no_telp`, `logo`) VALUES
@@ -140,23 +156,77 @@ INSERT INTO `tbl_profil` (`id`, `nama_sistem`, `alamat`, `provinsi`, `kabupaten`
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_skpa`
+-- Struktur dari tabel `tbl_skpa`
 --
 
 CREATE TABLE `tbl_skpa` (
-  `id_skpa` int(10) NOT NULL,
+  `id_skpa` int(11) NOT NULL,
   `username` varchar(25) NOT NULL,
-  `nama_skpa` varchar(255) NOT NULL,
-  `regulasi` varchar(100) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `level` varchar(10) NOT NULL,
-  `status` varchar(15) NOT NULL
-) ;
+  `password` varchar(100) NOT NULL,
+  `nama_skpa` varchar(100) NOT NULL,
+  `regulasi` varchar(70) NOT NULL,
+  `level` enum('user','admin') NOT NULL DEFAULT 'user',
+  `status` enum('aktif','non-aktif') NOT NULL DEFAULT 'non-aktif'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `tbl_skpa`
+--
+
+INSERT INTO `tbl_skpa` (`id_skpa`, `username`, `password`, `nama_skpa`, `regulasi`, `level`, `status`) VALUES
+(1, '', '', 'Sekretariat Daerah Aceh', 'Pergub Aceh No. 97 tahun 2016', 'user', 'non-aktif'),
+(2, '', '', 'Sekretariat Dewan Perwakilan Rakyat Aceh', 'Pergub Aceh No. 98 tahun 2016', 'user', 'non-aktif'),
+(3, '', '', 'Inspektorat Aceh', 'Pergub Aceh No. 99 tahun 2016', 'user', 'non-aktif'),
+(4, '', '', 'Badan Perencanaan Pembangunan Daerah Aceh', 'Pergub Aceh No. 67 tahun 2018', 'user', 'non-aktif'),
+(5, '', '', 'Badan Pengelolaan Keuangan Aceh', 'Pergub Aceh No. 66 tahun 2018', 'user', 'non-aktif'),
+(6, '', '', 'Badan Kepegawaian Aceh', 'Pergub Aceh No. 102 tahun 2016', 'user', 'non-aktif'),
+(7, '', '', 'Badan Pengembangan Sumber Daya manusia Aceh', 'Pergub Aceh No. 103 tahun 2016', 'user', 'non-aktif'),
+(8, '', '', 'Badan Penangulangan Bencana Aceh', 'Pergub Aceh No. 104 tahun 2016', 'user', 'non-aktif'),
+(9, '', '', 'Badan Penghubung Pemerintah Aceh', 'Pergub Aceh No. 105 tahun 2016', 'user', 'non-aktif'),
+(10, '', '', 'Dinas Pendidikan Aceh', 'Pergub Aceh No. 106 tahun 2016', 'user', 'non-aktif'),
+(11, '', '', 'Dinas Kesehatan Aceh', 'Pergub Aceh No. 107 tahun 2016', 'user', 'non-aktif'),
+(12, '', '', 'Dinas Pekerjaan Umum dan Penataan Ruang Aceh', 'Pergub Aceh No. 108 tahun 2016', 'user', 'non-aktif'),
+(13, '', '', 'Dinas Pengairan Aceh', 'Pergub Aceh No. 109 tahun 2016', 'user', 'non-aktif'),
+(14, '', '', 'Dinas Perumahan Rakyat Aceh', 'Pergub Aceh No. 110 tahun 2016', 'user', 'non-aktif'),
+(15, '', '', 'Dinas Sosial Aceh', 'Pergub Aceh No. 111 tahun 2016', 'user', 'non-aktif'),
+(16, '', '', 'Dinas Tenaga Kerja dan Mobilitas Penduduk Aceh', 'Pergub Aceh No. 112 tahun 2016', 'user', 'non-aktif'),
+(17, '', '', 'Dinas Pemberdayaan Perempuan dan Perlindungan Anak', 'Pergub Aceh No. 113 tahun 2016', 'user', 'non-aktif'),
+(18, '', '', 'Dinas Pangan Aceh', 'Pergub Aceh No. 114 tahun 2016', 'user', 'non-aktif'),
+(19, '', '', 'Dinas Lingkungan Hidup dan Pangan Aceh', 'Pergub Aceh No. 115 tahun 2016', 'user', 'non-aktif'),
+(20, '', '', 'Dinas registrasi Kependudukan Aceh', 'Pergub Aceh No. 116 tahun 2016', 'user', 'non-aktif'),
+(21, '', '', 'Dinas Pemberdayaan Masyarakat dan Gampong Aceh', 'Pergub Aceh No. 117 tahun 2016', 'user', 'non-aktif'),
+(22, '', '', 'Dinas Perhubungan Aceh', 'Pergub Aceh No. 118 tahun 2016', 'user', 'non-aktif'),
+(23, '', '', 'Dinas Komunikasi, Informatika dan Persandian Aceh', 'Pergub Aceh No. 119 tahun 2016', 'user', 'non-aktif'),
+(24, '', '', 'Dinas Koperasi dan Usaha Kecil Dan Menengah Aceh', 'Pergub Aceh No. 120 tahun 2016', 'user', 'non-aktif'),
+(25, '', '', 'Dinas Penanaman Modal Dan Pelayanan Terpadu Satu Pintu Aceh', 'Pergub Aceh No. 121 tahun 2016', 'user', 'non-aktif'),
+(26, '', '', 'Dinas Pemuda Dan Olahraga Aceh', 'Pergub Aceh No. 122 tahun 2016', 'user', 'non-aktif'),
+(27, '', '', 'Dinas Kebudayaan dan Pariwisata Aceh', 'Pergub Aceh No. 123 tahun 2016', 'user', 'non-aktif'),
+(28, '', '', 'Dinas Perpustakaan dan Kearsipan Aceh', 'Pergub Aceh No. 124 tahun 2016', 'user', 'non-aktif'),
+(29, '', '', 'Dinas Kelautan Dan Perikanan Aceh', 'Pergub Aceh No. 125 tahun 2016', 'user', 'non-aktif'),
+(30, '', '', 'Dinas Pertanian dan Perkebunan Aceh', 'Pergub Aceh No. 126 tahun 2016', 'user', 'non-aktif'),
+(31, '', '', 'Dinas Peternakan Aceh', 'Pergub Aceh No. 127 tahun 2016', 'user', 'non-aktif'),
+(32, '', '', 'Dinas Energi dan Sumber Daya Mineral Aceh', 'Pergub Aceh No. 128 tahun 2016', 'user', 'non-aktif'),
+(33, '', '', 'Dinas Perindustrian dan Perdagangan Aceh', 'Pergub Aceh No. 129 tahun 2016', 'user', 'non-aktif'),
+(34, '', '', 'Keurukon Katibul Wali', 'Pergub Aceh No. 130 tahun 2016', 'user', 'non-aktif'),
+(35, '', '', 'Dinas Syariat Islam Aceh', 'Pergub Aceh No. 131 tahun 2016', 'user', 'non-aktif'),
+(36, '', '', 'Dinas Pendidikan dayah Aceh', 'Pergub Aceh No. 132 tahun 2016', 'user', 'non-aktif'),
+(37, '', '', 'Dinas Pertanahan Aceh', 'Pergub Aceh No. 133 tahun 2016', 'user', 'non-aktif'),
+(38, '', '', 'Sekretariat Majelis Permusyawaratan Ulama Aceh', 'Pergub Aceh No. 134 tahun 2016', 'user', 'non-aktif'),
+(39, '', '', 'Sekretariat Majelis Adat Aceh', 'Pergub Aceh No. 135 tahun 2016', 'user', 'non-aktif'),
+(40, '', '', 'Sekretariat Majelis Pendidikan Aceh', 'Pergub Aceh No. 136 tahun 2016', 'user', 'non-aktif'),
+(41, '', '', 'Sekretariat Baitul Mal Aceh', 'Pergub Aceh No. 137 tahun 2016', 'user', 'non-aktif'),
+(42, '', '', 'Sekretariat Badan Reintegrasi', 'Pergub Aceh No. 138 tahun 2016', 'user', 'non-aktif'),
+(43, '', '', 'Satuan Polisi Pamong Praja dan Wilayatul Hisbah Aceh', 'Pergub Aceh No. 139 tahun 2016', 'user', 'non-aktif'),
+(44, '', '', 'Rumah Sakit Umum dr. Zainoel Abidin', 'Pergub Aceh No. 140 tahun 2016', 'user', 'non-aktif'),
+(45, '', '', 'Rumah Sakit Jiwa', 'Pergub Aceh No. 141 tahun 2016', 'user', 'non-aktif'),
+(46, '', '', 'Rumah Sakit Ibu dan Anak', 'Pergub Aceh No. 142 tahun 2016', 'user', 'non-aktif'),
+(47, '', '', 'Badan Kesatuan Bangsa dan Politik Aceh', 'Pergub Aceh No. 143 tahun 2016', 'user', 'non-aktif'),
+(48, 'admin', '21232f297a57a5a743894a0e4a801fc3', '', '', 'admin', 'aktif');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_suborganisasi`
+-- Struktur dari tabel `tbl_suborganisasi`
 --
 
 CREATE TABLE `tbl_suborganisasi` (
@@ -165,7 +235,14 @@ CREATE TABLE `tbl_suborganisasi` (
   `nama_suborganisasi` varchar(200) NOT NULL,
   `tugas` varchar(2000) NOT NULL,
   `fungsi` varchar(2000) NOT NULL
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `tbl_suborganisasi`
+--
+
+INSERT INTO `tbl_suborganisasi` (`id_suborganisasi`, `id_organisasi`, `nama_suborganisasi`, `tugas`, `fungsi`) VALUES
+(1, 4, 'tes', '<ol><li>f</li><li>g</li><li>l</li><li>&nbsp;</li></ol>', '<ol><li>a</li><li>b<ol><li>v</li></ol></li></ol>');
 
 --
 -- Indexes for dumped tables
@@ -176,12 +253,6 @@ CREATE TABLE `tbl_suborganisasi` (
 --
 ALTER TABLE `captcha`
   ADD PRIMARY KEY (`captcha_id`);
-
---
--- Indexes for table `tbl_ded`
---
-ALTER TABLE `tbl_ded`
-  ADD PRIMARY KEY (`id_ded`);
 
 --
 -- Indexes for table `tbl_dokumen`
@@ -196,10 +267,22 @@ ALTER TABLE `tbl_note`
   ADD PRIMARY KEY (`id_note`);
 
 --
+-- Indexes for table `tbl_objek_data`
+--
+ALTER TABLE `tbl_objek_data`
+  ADD PRIMARY KEY (`id_objek_data`);
+
+--
 -- Indexes for table `tbl_organisasi`
 --
 ALTER TABLE `tbl_organisasi`
   ADD PRIMARY KEY (`id_organisasi`);
+
+--
+-- Indexes for table `tbl_parameter`
+--
+ALTER TABLE `tbl_parameter`
+  ADD PRIMARY KEY (`id_parameter`);
 
 --
 -- Indexes for table `tbl_profil`
@@ -227,51 +310,47 @@ ALTER TABLE `tbl_suborganisasi`
 -- AUTO_INCREMENT for table `captcha`
 --
 ALTER TABLE `captcha`
-  MODIFY `captcha_id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tbl_ded`
---
-ALTER TABLE `tbl_ded`
-  MODIFY `id_ded` int(15) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `captcha_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 --
 -- AUTO_INCREMENT for table `tbl_dokumen`
 --
 ALTER TABLE `tbl_dokumen`
-  MODIFY `id_dokumen` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id_dokumen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `tbl_note`
 --
 ALTER TABLE `tbl_note`
-  MODIFY `id_note` int(10) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id_note` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `tbl_objek_data`
+--
+ALTER TABLE `tbl_objek_data`
+  MODIFY `id_objek_data` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `tbl_organisasi`
 --
 ALTER TABLE `tbl_organisasi`
-  MODIFY `id_organisasi` int(15) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id_organisasi` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `tbl_parameter`
+--
+ALTER TABLE `tbl_parameter`
+  MODIFY `id_parameter` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `tbl_profil`
 --
 ALTER TABLE `tbl_profil`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `tbl_skpa`
 --
 ALTER TABLE `tbl_skpa`
-  MODIFY `id_skpa` int(10) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id_skpa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 --
 -- AUTO_INCREMENT for table `tbl_suborganisasi`
 --
 ALTER TABLE `tbl_suborganisasi`
-  MODIFY `id_suborganisasi` int(15) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
+  MODIFY `id_suborganisasi` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
